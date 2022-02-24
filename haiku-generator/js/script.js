d35875acc1868571ffb529b523b7a77c00bfb3f4
1huali <wanhuali2000@gmail.com>
2022-02-23 21:41:48 -0500

    Haiku generated randomly w random(array); and prints in console

--- haiku-generator/js/script.js
+++ haiku-generator/js/script.js
@@ -6,27 +6,31 @@ Haiku generator from html
 */
 
 "use strict";
+let fiveSyllableLines= [
+  `0, to be a tree`,
+  `the cat does not know`,
+  `We are all forests`,
+  `You have done your best`,
+  `They are all gone now`
+];
 
+let sevenSyllableLines= [
+  `Say the things left unsaid`,
+  `Never believe the wind's lies`,
+  `The autumn stretches its legs`,
+  `Nothing can satisfy you`,
+  `They will not come back again`
+]
 
-/**
-Description of preload
-*/
-function preload() {
+let line1= random(fiveSyllableLines);
+let line2= random(sevenSyllableLines);
+let line3=random(fiveSyllableLines);
 
-}
+console.log(line1);
+console.log(line2);
+console.log(line3);
 
-
-/**
-Description of setup
-*/
-function setup() {
-
-}
-
-
-/**
-Description of draw()
-*/
-function draw() {
-
+function random(array){
+  let index = Math.floor(Math.random() * array.length);
+  return array [index];
 }
