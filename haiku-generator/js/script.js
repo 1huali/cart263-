53552726f6435dfaa37431ec053537f50656a5b5
1huali <wanhuali2000@gmail.com>
2022-02-23 21:01:21 -0500

    created js file

--- haiku-generator/js/script.js
+++ haiku-generator/js/script.js
@@ -1,9 +1,8 @@
 /**
-Title of Project
-Author Name
+Haiku generator
+Wawa Li
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
+Haiku generator from html
 */
 
 "use strict";
