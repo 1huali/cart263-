6489856b93fdc0e56cae82e15eedc8d3e4380cce
1huali <wanhuali2000@gmail.com>
2022-02-24 09:14:19 -0500

    update the lineClicked to a fadeOut() - tho it bugs

--- haiku-generator/js/script.js
+++ haiku-generator/js/script.js
@@ -36,11 +36,48 @@ printLine1.innerText = line1;
 printLine2.innerText = line2;
 printLine3.innerText = line3;
 
-function displayHaiku(){
+//activates and associates the clicks to an event
+// printLine1.addEventListener('click', lineClicked);
+// printLine2.addEventListener('click', lineClicked);
+// printLine3.addEventListener('click', lineClicked);
+
+// function that generates a random line in five or seven lines depending on the verse
+function setNewLine(element){
+  if (element === printLine1 || element === printLine3){
+    element.innerText = random(fiveSyllableLines);
+  }
+  else if(element === printLine2){
+    element.innerText = random(sevenSyllableLines);
+  }
 }
 
+//line is set when user clicks on the line
+// Q: what is event target
+function lineClicked(event){
+// setNewLine(event.target);
+fadeOut(event.target, 1);
+}
+
+//adds fade-in and fade-out transition effect
+//i dont understand what happened here
+function fadeOut(element, opacity){
+  opacity -= 0.01;
+  element.style[`opacity`] = opacity;
+  if (opacity > 0){
+    requestAnimationframe (function (){
+      fadeOut(element, opacity);
+    });
+  }
+  // else {
+  //   //something else when faded out
+  // }
+}
 
 function random(array){
   let index = Math.floor(Math.random() * array.length);
   return array [index];
 }
+
+printLine1.addEventListener('click', fadeOut);
+printLine2.addEventListener('click', fadeOut);
+printLine3.addEventListener('click', fadeOut);
