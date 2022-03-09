cb3260863a9a0584e73d89656edc7ae6b7a53e6a
1huali <wanhuali2000@gmail.com>
2022-03-09 16:43:47 -0500

    Letters change color when mouseover

--- code-taker/js/script.js
+++ code-taker/js/script.js
@@ -1,11 +1,12 @@
 /**
-Title of Project
-Author Name
+Da Vinci
+Wawa Li
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
+jQuery exercice
 */
 
 "use strict";
 
-// Code goes here
+$(`.secret`).on(`mouseover`, function(event){
+$(this).addClass(`found`,500);
+});
