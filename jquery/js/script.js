70d411fa1ce3bff523bff71fbdb3ad10fd829b3d
1huali <wanhuali2000@gmail.com>
2022-03-09 11:11:55 -0500

    Added shaky effect to the prisonner

--- jquery/js/script.js
+++ jquery/js/script.js
@@ -6,14 +6,27 @@ This is a template. You must fill in the title,
 author, and this description to match your project!
 */
 
+$(`#prisoner`).effects({
+  effect: `shake`,
+  duration: 2000,
+  times: 15,
+  distance: 7
+});
+
 // think jQuery doesnt work
 $(`#prisoner`).draggable({
     containment: `#prison`,
     start: function(event,ui){
       $(this).css(`text-decoration`,`underline`);
+      $(this).animate({
+        `color`: `#4444ff`
+      },750);
     },
       stop: function(event,ui){
         $(this).css(`text-decoration`,`none`);
+        $(this).animate({
+          `color`: `#black`
+        },750);
       }
   });
 
