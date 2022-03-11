61b738917de4ba7c56cfed9840d1d0c14d96d9d7
1huali <wanhuali2000@gmail.com>
2022-03-11 16:39:48 -0500

    Update : concision of code

--- mulan-poem/js/script.js
+++ mulan-poem/js/script.js
@@ -6,152 +6,198 @@ Mulan poem
 */
 
 "use strict";
-let state = `title`;
+// let state = `title`;
 let song;
 let daggerSound;
 changingState();
+let currentPageNumber=0;
+
 
 // doesnt get the song
-song = document.getElementById("song").innerHTML;
+song = document.getElementById("song");
 daggerSound = document.getElementById("sound1");
-setTimeout(openPoem,1000);
+openPoem();
 
 // dagger sounds when hover over some random letters
 $(`.hover`).on(`mouseover`, function(event){
-    // $(this).addClass(`found`,500);
     daggerSound.play();
     });
 
+
 function openPoem(){
-    state = `part1`;
-    song.play();
-    //song doesnt play
+ $(`#part0`).on(`click`, function(event){
+     console.log(song.duration);
+// source : https://stackoverflow.com/questions/9437228/html5-check-if-audio-is-playing
+if (song.duration > 0 && !song.paused) {
+} else {
+    // song.play();
+}
+            });
+    
+
 }
 
 function changingState(){
-    // if (state === `title`){
-    //     $(`#poem`).children().hide();
-    //     $(`#title`).on(`click`, function(event){
-    //         state = `part1`;
-    //         changingState();
-    //         console.log(state);
-    //         });
-    // }
-if (state === `part1`){
-    $(`#poem`).children().hide();
-    $(`#partOne-poem`).show(`blind`);
-    $(`#partOne-poem`).on(`click`, function(event){
-        state = `part2`;
-        changingState();
-        console.log(`poem p.1`)
-        console.log(state);
+    let poemSections = $(`.poemSection`);
+    console.log(poemSections);
+    //parts are made into html array
+    for (let i=0;i < poemSections.length; i++){
+        // tells which part we are on atm
+    let currentPart = poemSections[i].id
+
+    // I wanted the poem to change back to black every page but yeah
+    $(`#poem`).on(`mouseover`, function(event){
+        $(`#part${i}`).addClass(`read`,5000);
+    });
+
+    // tells which is the upcoming part
+    let nextPart = poemSections[i].getAttribute("nextSection");
+   // console.log(currentPart);
+
+    // switch to next page when user clicks on the displayed part (nextPart displays)
+$(`#${currentPart}`).on(`click`, function(event){
+        $(`#poem`).children().hide();
+        $(`#part${nextPart}`).show(`blind`);
+        // console.log(nextPart)
+        currentPageNumber = parseInt(nextPart);
+        $(`#poem`).on(`click`, function(event){
+            $(`poem`).removeClass(`read`,500);
         });
+});
+    }
+
+    $("#next").on('click',function(event){
+        console.log(currentPageNumber);
+        $(`#poem`).children().hide();
+        $(`#part${currentPageNumber+1}`).show(`blind`);
+        currentPageNumber =currentPageNumber+1;
+        //console.log(nextPart)
+        //currentPageNumber = parseInt(nextPart);
+        $(`#poem`).on(`click`, function(event){
+            $(`#poem`).removeClass(`read`,500);
+        });
+
+    })
+
+
+
+// else if (state === `part2`){
+//     $(`#poem`).children().hide();
+//     $(`#part2`).show(`blind`);
+//     console.log(`poem p.2`)
+//     $(`#part2`).on(`click`, function(event){
+//         state = `part3`
+//         changingState();
+//         });
+// }
+
+// else if (state === `part3`){
+//     $(`#poem`).children().hide();
+//     $(`#partThree-poem`).show(`blind`);
+//     console.log(`poem p.3`)
+//     $(`#partThree-poem`).on(`click`, function(event){
+//         state = `part4`
+//         changingState();
+//         });
+// }
+
+// else if (state === `part4`){
+//     $(`#poem`).children().hide();
+//     $(`#partFour-poem`).show(`blind`);
+//     console.log(`poem p.4`)
+//     $(`#partFour-poem`).on(`click`, function(event){
+//         state = `part5`
+//         changingState();
+//         });
+// }
+// else if (state === `part5`){
+//     $(`#poem`).children().hide();
+//     $(`#partFive-poem`).show(`blind`);
+//     console.log(`poem p.5`)
+//     $(`#partFive-poem`).on(`click`, function(event){
+//         state = `part6`
+//         changingState();
+//         });
+// }
+// else if (state === `part6`){
+//     $(`#poem`).children().hide();
+//     $(`#partSix-poem`).show(`blind`);
+//     console.log(`poem p.6`)
+//     $(`#partSix-poem`).on(`click`, function(event){
+//         state = `part7`
+//         changingState();
+//         });
+// }
+// else if (state === `part7`){
+//     $(`#poem`).children().hide();
+//     $(`#partSeven-poem`).show(`blind`);
+//     console.log(`poem p.7`)
+//     $(`#partSeven-poem`).on(`click`, function(event){
+//         state = `part8`
+//         changingState();
+//         });
+// }
+// else if (state === `part8`){
+//     $(`#poem`).children().hide();
+//     $(`#partEight-poem`).show(`blind`);
+//     console.log(`poem p.8`)
+//     $(`#partEight-poem`).on(`click`, function(event){
+//         state = `part9`
+//         changingState();
+//         });
+// }
+// else if (state === `part9`){
+//     $(`#poem`).children().hide();
+//     $(`#partNine-poem`).show(`blind`);
+//     console.log(`poem p.9`)
+//     $(`#partNine-poem`).on(`click`, function(event){
+//         state = `part10`
+//         changingState();
+//         });
+// }
+// else if (state === `part10`){
+//     $(`#poem`).children().hide();
+//     $(`#partTen-poem`).show(`blind`);
+//     console.log(`poem p.10`)
+//     $(`#partTen-poem`).on(`click`, function(event){
+//         state = `part11`
+//         changingState();
+//         });
+// }
+// else if (state === `part11`){
+//     $(`#poem`).children().hide();
+//     $(`#partEleven-poem`).show(`blind`);
+//     console.log(`poem p.11`)
+//     $(`#partEleven-poem`).on(`click`, function(event){
+//         state = `part12`
+//         changingState();
+//         });
+// }
+// else if (state === `part12`){
+//     $(`#poem`).children().hide();
+//     $(`#partTwelve-poem`).show(`blind`);
+//     console.log(`poem p.12`)
+//     $(`#partTwelve-poem`).on(`click`, function(event){
+//         state = `part13`
+//         changingState();
+//         });
+// }
+
 }
 
-else if (state === `part2`){
-    $(`#poem`).children().hide();
-    $(`#partTwo-poem`).show(`blind`);
-    console.log(`poem p.2`)
-    $(`#partTwo-poem`).on(`click`, function(event){
-        state = `part3`
-        changingState();
-        });
-}
+// $( "button" ).click( function( event ) {
+//     event.goBack;
+//   } );
 
-else if (state === `part3`){
-    $(`#poem`).children().hide();
-    $(`#partThree-poem`).show(`blind`);
-    console.log(`poem p.3`)
-    $(`#partThree-poem`).on(`click`, function(event){
-        state = `part4`
-        changingState();
-        });
-}
+//   function goBack(){
+//       if (state=== `part2`){
+//           state= 3;
+//       } else if (state=== `part3`){
+//           state =2;
+//       } else if (state===)
 
-else if (state === `part4`){
-    $(`#poem`).children().hide();
-    $(`#partFour-poem`).show(`blind`);
-    console.log(`poem p.4`)
-    $(`#partFour-poem`).on(`click`, function(event){
-        state = `part5`
-        changingState();
-        });
-}
-else if (state === `part5`){
-    $(`#poem`).children().hide();
-    $(`#partFive-poem`).show(`blind`);
-    console.log(`poem p.5`)
-    $(`#partFive-poem`).on(`click`, function(event){
-        state = `part6`
-        changingState();
-        });
-}
-else if (state === `part6`){
-    $(`#poem`).children().hide();
-    $(`#partSix-poem`).show(`blind`);
-    console.log(`poem p.6`)
-    $(`#partSix-poem`).on(`click`, function(event){
-        state = `part7`
-        changingState();
-        });
-}
-else if (state === `part7`){
-    $(`#poem`).children().hide();
-    $(`#partSeven-poem`).show(`blind`);
-    console.log(`poem p.7`)
-    $(`#partSeven-poem`).on(`click`, function(event){
-        state = `part8`
-        changingState();
-        });
-}
-else if (state === `part8`){
-    $(`#poem`).children().hide();
-    $(`#partEight-poem`).show(`blind`);
-    console.log(`poem p.8`)
-    $(`#partEight-poem`).on(`click`, function(event){
-        state = `part9`
-        changingState();
-        });
-}
-else if (state === `part9`){
-    $(`#poem`).children().hide();
-    $(`#partNine-poem`).show(`blind`);
-    console.log(`poem p.9`)
-    $(`#partNine-poem`).on(`click`, function(event){
-        state = `part10`
-        changingState();
-        });
-}
-else if (state === `part10`){
-    $(`#poem`).children().hide();
-    $(`#partTen-poem`).show(`blind`);
-    console.log(`poem p.10`)
-    $(`#partTen-poem`).on(`click`, function(event){
-        state = `part11`
-        changingState();
-        });
-}
-else if (state === `part11`){
-    $(`#poem`).children().hide();
-    $(`#partEleven-poem`).show(`blind`);
-    console.log(`poem p.11`)
-    $(`#partEleven-poem`).on(`click`, function(event){
-        state = `part12`
-        changingState();
-        });
-}
-else if (state === `part12`){
-    $(`#poem`).children().hide();
-    $(`#partTwelve-poem`).show(`blind`);
-    console.log(`poem p.12`)
-    $(`#partTwelve-poem`).on(`click`, function(event){
-        state = `part13`
-        changingState();
-        });
-}
-
-}
-
+//       }
+//   }
 // previous / next button
 // $(`#buttons`).button({`<`: value1, `>`: value2 });
 
