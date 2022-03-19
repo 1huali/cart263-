8a2a7f426d5ea5f8033dbdda3234e67215d2d346
1huali <wanhuali2000@gmail.com>
2022-03-19 19:56:00 -0400

    To fix : bgImg, states, score, countdown

--- green-game/js/script.js
+++ green-game/js/script.js
@@ -8,9 +8,50 @@ so they click the secret text to restore the redaction
 */
 
 "use strict";
+let state= `intro`;
 let bank=0;
 // gain/point counter
 
+//intro box dialog
+//Q: how to center button
+$(`#intro-dialog`).dialog({
+    modal: true,
+    buttons: {
+        "green" : function(){
+            $(this).dialog(`close`);
+            state= `game`;
+        }
+    }
+});
+
+//30 second game
+if (state === `game`){
+setTimeout (game, 30000);
+// } else {
+//     state = `end`;
+}
+
+//decide wtv happens at the end
+// if (state=== `end`){
+// $(`#end-dialog`).dialog({
+//     buttons: {
+//         "good night" : function(){
+//             $(this).dialog(`close`);
+//         }
+//     }
+// });
+// }
+
+function game(){
+    state= `game`;
+// money reveals and hide randomly
+setInterval (revelation, 500);
+setInterval (hide, 200);
+displayBank();
+scoreIncrease();
+}
+
+function scoreIncrease(){
 //when clicked, money becomes a +1 in bank and .gain class
 $(`.liquidity`).on(`click`, function(event){
     $(this).addClass(`gain`);
@@ -20,28 +61,25 @@ $(`.liquidity`).on(`click`, function(event){
     console.log("+1!!")
     console.log(bank);
 });
+}
 
 // Q: how to display DOESNT WORK
 function displayBank (){
-        $(`.liquidity`).click(function() {
-          $(`#moneyGain`).text($(bank));
-        });
-    // $(`#moneyGain`).text(`blind`);
+    $(`.liquidity`).click(function() {
+      $(`#moneyGain`).text($(bank));
+    });
+// $(`#moneyGain`).text(`blind`);
 }
 
 // +1 text replaces the flying dollar icon for 1 second when user clicks (gratification feature) 
 //DOESNT WORK
 function plusOneSign(){
-    setTimeout(function() {
+setTimeout(function() {
 $(`.revealed`).text("+1");
 $(`img`).text("+1");
-        }, 600);
-      };
+    }, 600);
+  };
 
-
-// money reveals and hide randomly
-setInterval (revelation, 500);
-setInterval (hide, 200);
 
 function revelation(){
     $(`.hidden`).each(function(){
