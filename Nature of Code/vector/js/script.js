0b03792cb6e49ef6f61347ca3d89fb49e3a24dff
1huali <wanhuali2000@gmail.com>
2022-03-22 13:15:35 -0400

    replaced random() path direction to velocity vector

--- Nature of Code/vector/js/script.js
+++ Nature of Code/vector/js/script.js
@@ -13,10 +13,10 @@ let walker;
 function setup() {
   createCanvas(400, 400);
   walker = new Walker(200, 200);
-  background(0);
 }
 
 function draw() {
-  walker.update();
+    background(0);
+    walker.update();
   walker.show();
 }
