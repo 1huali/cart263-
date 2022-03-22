d7e1e56d31bb09791dcf2905901c470f3c4875e9
1huali <wanhuali2000@gmail.com>
2022-03-22 19:43:19 -0400

    update

--- Nature of Code/mover/js/Mover.js
+++ Nature of Code/mover/js/Mover.js
@@ -1,38 +1,44 @@
 class Mover {
-    constructor(x, y) {
-      this.pos = createVector(x, y);
-      //adding velocity vector
-    //   this.vel = createVector(200,100);
+  constructor(x, y) {
+    this.pos = createVector(x, y);
+    this.vel = createVector(0, 0);
+    this.acc = createVector(0, 0);
+    this.r = 16;
+  }
 
-    //   randomized velocity vector
-      this.vel = p5.Vector.random2D();
-      
-    
+  applyForce(force) {
+    this.acc.add(force);
+  }
+
+  edges() {
+    if (this.pos.y >= height - this.r) {
+      this.pos.y = height - this.r;
+      this.vel.y *= -1;
     }
-  
-    update() {
-    //   this.pos.x = this.pos.x + random(-1, 1);
-    //   this.pos.y = this.pos.y + random(-1, 1);
 
-    //replacing velocity vector as a direction
-          this.pos.add(this.vel);
-
-    //random vector
-    translate(width/2,height/2);
-
-        // this.vel =  createVector(random(-100,100),random(-100,100));
-    } 
-  
-    show() {
-      stroke(255);
-      strokeWeight(2);
-      fill(255,100);
-
-    //   diff form
-      ellipse(this.pos.x,this.pos.y,32);
-    //   point(this.pos.x, this.pos.y);
-    // line(0,0,this.pos.x, this.pos.y);
+    if (this.pos.x >= width - this.r) {
+      this.pos.x = width - this.r;
+      this.vel.x *= -1;
+    } else if (this.pos.x <= this.r) {
+      this.pos.x = this.r;
+      this.vel.x *= -1;
     }
   }
 
+  update() {
+    // let mouse = createVector(mouseX, mouseY);
+    // this.acc = p5.Vector.sub(mouse, this.pos);
+    // this.acc.setMag(0.1);
 
+    this.vel.add(this.acc);
+    this.pos.add(this.vel);
+    this.acc.set(0, 0);
+  }
+
+  show() {
+    stroke(255);
+    strokeWeight(2);
+    fill(255, 100);
+    ellipse(this.pos.x, this.pos.y, this.r * 2);
+  }
+}
--- Nature of Code/mover/js/script.js
+++ Nature of Code/mover/js/script.js
@@ -7,16 +7,25 @@ Wawa Li
 // https://thecodingtrain.com/learning/nature-of-code/1.1-what-is-a-vector.html
 // https://editor.p5js.org/codingtrain/sketches/JmEToUfk
 */
-
-let walker;
+let mover;
 
 function setup() {
   createCanvas(400, 400);
-  walker = new Walker(200, 200);
+  mover = new Mover(200, 200);
 }
 
 function draw() {
-    background(0);
-    walker.update();
-  walker.show();
+  background(0);
+
+  if (mouseIsPressed) {
+    let wind = createVector(0.1, 0);
+    mover.applyForce(wind);
+  }
+
+  let gravity = createVector(0, 0.2);
+  mover.applyForce(gravity);
+
+  mover.update();
+  mover.edges();
+  mover.show();
 }
