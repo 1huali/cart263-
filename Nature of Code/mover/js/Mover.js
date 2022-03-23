597aaebf9b9fc8692c007e7ac4f32442a8b3f529
1huali <wanhuali2000@gmail.com>
2022-03-23 19:45:24 -0400

    friction theory

--- Nature of Code/mover/js/Mover.js
+++ Nature of Code/mover/js/Mover.js
@@ -1,13 +1,38 @@
 class Mover {
-  constructor(x, y) {
+  constructor(x, y, m) {
     this.pos = createVector(x, y);
     this.vel = createVector(0, 0);
     this.acc = createVector(0, 0);
-    this.r = 16;
+    this.mass = m;
+    this.r = sqrt(this.mass) * 10;
+  }
+
+  friction() {
+        //check the direction of the friction (normalized velocity*opposite direction of force) and the magnitude of the friction (mu*normal)
+
+    let diff = height - (this.pos.y + this.r);
+    if (diff < 1) {
+      // shorcut to friction!
+      // this.vel.mult(0.95);
+
+      // Direction of Friction
+      let friction = this.vel.copy();
+      friction.normalize();
+      friction.mult(-1);
+
+      // chained version
+      // let friction = this.vel.copy().normalize().mult(-1);
+
+      // Magnitude of Friction
+      let normal = this.mass;
+      friction.setMag(mu * normal);
+      this.applyForce(friction);
+    }
   }
 
   applyForce(force) {
-    this.acc.add(force);
+    let f = p5.Vector.div(force, this.mass);
+    this.acc.add(f);
   }
 
   edges() {
--- Nature of Code/mover/js/script.js
+++ Nature of Code/mover/js/script.js
@@ -7,25 +7,30 @@ Wawa Li
 // https://thecodingtrain.com/learning/nature-of-code/1.1-what-is-a-vector.html
 // https://editor.p5js.org/codingtrain/sketches/JmEToUfk
 */
-let mover;
+let movers = [];
+let mu = 0.1;
 
 function setup() {
   createCanvas(400, 400);
-  mover = new Mover(200, 200);
+  for (let i = 0; i < 10; i++) {
+    movers[i] = new Mover(random(width), 200, random(1, 8));
+  }
 }
 
 function draw() {
   background(0);
 
-  if (mouseIsPressed) {
-    let wind = createVector(0.1, 0);
-    mover.applyForce(wind);
+  for (let mover of movers) {
+    if (mouseIsPressed) {
+      let wind = createVector(0.1, 0);
+      mover.applyForce(wind);
+    }
+    let gravity = createVector(0, 0.2);
+    let weight = p5.Vector.mult(gravity, mover.mass);
+    mover.applyForce(weight);
+    mover.friction();
+    mover.update();
+    mover.edges();
+    mover.show();
   }
-
-  let gravity = createVector(0, 0.2);
-  mover.applyForce(gravity);
-
-  mover.update();
-  mover.edges();
-  mover.show();
 }
